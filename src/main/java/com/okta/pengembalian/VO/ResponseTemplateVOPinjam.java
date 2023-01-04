377aed916daa365439ef55f90774280660d4f1eb
Oktavianipratiwi <113502499+Oktavianipratiwi@users.noreply.github.com>
2023-01-04 23:09:13 +0700

    Add files via upload

---
+++ src/main/java/com/okta/pengembalian/VO/ResponseTemplateVOPinjam.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.okta.pengembalian.VO;
+
+import com.okta.pengembalian.VO.Buku;
+import com.okta.pengembalian.VO.Anggota;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author 
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVOPinjam {
+    private Peminjaman peminjaman;
+    private Anggota anggota;
+    private Buku buku;
+}
